21125cac2a9cfd802f596a4b3e85db9ddae01099
Stephen <stephen.pierre20@gmail.com>
2018-02-26 00:18:33 +0000

    all the code from the javascripting workshop

---
+++ function-arguments.js
@@ -0,0 +1,7 @@
+function math (r1,r2,r3){
+
+	return (r2 * r3) + r1 ;
+}
+
+console.log(math(53,61,67));
+
---
+++ if-statement.js
@@ -0,0 +1,11 @@
+var fruit = "orange";
+
+if (fruit.length > 5){
+
+	console.log('The fruit name has more than five characters.');
+
+}else{
+
+	console.log('The friut name has less than five characters.');
+}
+
